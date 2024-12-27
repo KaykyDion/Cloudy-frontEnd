e4157fb79f0cb9e6341c895a03ca3466b71040c0
Kayky Dion <kayky.dion@gmail.com>
2024-12-27 11:15:20 -0300

    feat: project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Box, Heading } from "@radix-ui/themes";
+
+function App() {
+  return (
+    <Box maxWidth="80rem" mx="auto">
+      <Box>
+        <Heading as="h1" size="8" weight="light">
+          Cloudy
+        </Heading>
+      </Box>
+    </Box>
+  );
+}
+
+export default App;
